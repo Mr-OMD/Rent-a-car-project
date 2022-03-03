5597d54cb9532843ffe71ed61c4022da894aa3a2
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-03-03 23:43:17 +0300

    carService updated and fixed

--- rentacar/src/main/java/com/omertdemirel/rentacar/business/abstracts/CarService.java
+++ rentacar/src/main/java/com/omertdemirel/rentacar/business/abstracts/CarService.java
@@ -1,22 +1,30 @@
 package com.omertdemirel.rentacar.business.abstracts;
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
+
 import com.omertdemirel.rentacar.business.dtos.CarDto;
 import com.omertdemirel.rentacar.business.dtos.ListCarDto;
 import com.omertdemirel.rentacar.business.request.CreateCarRequest;
 import com.omertdemirel.rentacar.business.request.DeleteCarRequest;
 import com.omertdemirel.rentacar.business.request.UpdateCarRequest;
+import com.omertdemirel.rentacar.core.utilities.results.DataResult;
+import com.omertdemirel.rentacar.core.utilities.results.Result;
 
 public interface CarService {
-	List<ListCarDto> getAll();
+	DataResult<List<ListCarDto>> getAll();
 
-	CarDto getById(int id);
+	DataResult<CarDto> getById(int id);
 
-	void add(CreateCarRequest createCarRequest);
+	Result add(CreateCarRequest createCarRequest);
 
-	void delete(DeleteCarRequest deleteCarRequest);
+	Result delete(DeleteCarRequest deleteCarRequest);
 
-	void update(UpdateCarRequest updateCarRequest);
+	Result update(UpdateCarRequest updateCarRequest);
 
-	void delete(int carId);
+	DataResult<List<ListCarDto>> getByDailyPriceLessThan(double maxDailyPrice);
+
+	DataResult<List<ListCarDto>> getAllPaged(int pageNo, int pageSize);
+
+	DataResult<List<ListCarDto>> getAllSorted(Sort.Direction direction);
 }
