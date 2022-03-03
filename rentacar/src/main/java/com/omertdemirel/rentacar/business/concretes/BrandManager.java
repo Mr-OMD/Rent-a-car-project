1e514ac3e6870716e6eb9a2dab0e618ba6886a19
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-03-03 23:48:46 +0300

    brandManager updated

--- rentacar/src/main/java/com/omertdemirel/rentacar/business/concretes/BrandManager.java
+++ rentacar/src/main/java/com/omertdemirel/rentacar/business/concretes/BrandManager.java
@@ -8,20 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.omertdemirel.rentacar.business.abstracts.BrandService;
-
+import com.omertdemirel.rentacar.business.dtos.BrandDto;
 import com.omertdemirel.rentacar.business.dtos.ListBrandDto;
 import com.omertdemirel.rentacar.business.request.CreateBrandRequest;
 import com.omertdemirel.rentacar.business.request.DeleteBrandRequest;
 import com.omertdemirel.rentacar.business.request.UpdateBrandRequest;
 import com.omertdemirel.rentacar.core.utilities.mapping.ModelMapperService;
+import com.omertdemirel.rentacar.core.utilities.results.DataResult;
+import com.omertdemirel.rentacar.core.utilities.results.ErrorResult;
+import com.omertdemirel.rentacar.core.utilities.results.Result;
+import com.omertdemirel.rentacar.core.utilities.results.SuccessDataResult;
+import com.omertdemirel.rentacar.core.utilities.results.SuccessResult;
 import com.omertdemirel.rentacar.dataAccess.abstracts.BrandDao;
 import com.omertdemirel.rentacar.entities.concretes.Brand;
 
 @Service
 public class BrandManager implements BrandService{
 
-	private BrandDao brandDao;
-	private ModelMapperService modelMapperService;
+	private final BrandDao brandDao;
+	private final ModelMapperService modelMapperService;
 
 	@Autowired
 	public BrandManager(BrandDao brandDao, ModelMapperService modelMapperService) {
@@ -30,49 +35,48 @@ public class BrandManager implements BrandService{
 	}
 
 	@Override
-	public List<ListBrandDto> getAll() {
+	public DataResult<List<ListBrandDto>> getAll() {
 		List<Brand> result = brandDao.findAll();
 		List<ListBrandDto> response = result.stream()
 				.map(brand -> modelMapperService.forDto().map(brand, ListBrandDto.class)).collect(Collectors.toList());
-		return response;
+		return new SuccessDataResult<List<ListBrandDto>>(response);
 	}
 
 	@Override
-	public void add(CreateBrandRequest createBrandRequest) {
-		Brand brand = modelMapperService.forRequest().map(createBrandRequest, Brand.class);
-		if (!doesExist(brand)) {
+	public Result add(CreateBrandRequest createBrandRequest) {
+		if (!brandDao.existsByBrandName(createBrandRequest.getBrandName())) {
+			Brand brand = modelMapperService.forRequest().map(createBrandRequest, Brand.class);
 			brandDao.save(brand);
+			return new SuccessResult();
 		}
-
+		return new ErrorResult("The brand already exist!");
 	}
 
 	@Override
-	public ListBrandDto getById(int id) {
+	public DataResult<BrandDto> getById(int id) {
 		Brand brand = brandDao.getById(id);
-		ListBrandDto response = modelMapperService.forDto().map(brand, ListBrandDto.class);
-		return response;
-	}
-
-	private boolean doesExist(Brand brand) {
-
-		return Objects.nonNull(brandDao.getByBrandName(brand.getBrandName()));
+		BrandDto response = modelMapperService.forDto().map(brand, BrandDto.class);
+		return new SuccessDataResult<BrandDto>(response);
 	}
 
 	@Override
-	public void update(UpdateBrandRequest updateBrandRequest) {
+	public Result delete(DeleteBrandRequest deleteBrandRequest) {
+		if (brandDao.existsById(deleteBrandRequest.getBrandId())) {
+			brandDao.deleteById(deleteBrandRequest.getBrandId());
+			return new SuccessResult();
+		}
+		return new ErrorResult("The brand was not found!");
+	}
+
+	@Override
+	public Result update(UpdateBrandRequest updateBrandRequest) {
 		if (brandDao.existsById(updateBrandRequest.getBrandId())) {
 			Brand brand = modelMapperService.forRequest().map(updateBrandRequest, Brand.class);
 			brandDao.save(brand);
+			return new SuccessResult();
 		}
-		
-	}
+		return new ErrorResult("The brand was not found!");
 
-	@Override
-	public void delete(DeleteBrandRequest deleteBrandRequest) {
-		if (brandDao.existsById(deleteBrandRequest.getBrandId())) {
-			brandDao.deleteById(deleteBrandRequest.getBrandId());
-		}
-		
 	}
 
 }
