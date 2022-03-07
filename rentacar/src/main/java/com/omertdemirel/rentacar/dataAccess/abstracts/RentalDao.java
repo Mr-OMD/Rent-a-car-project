30e352806a583410c71b53228078de6b681dce3b
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-03-07 17:48:06 +0300

    rental dao added

---
+++ rentacar/src/main/java/com/omertdemirel/rentacar/dataAccess/abstracts/RentalDao.java
@@ -0,0 +1,12 @@
+package com.omertdemirel.rentacar.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.omertdemirel.rentacar.entities.concretes.Rental;
+
+@Repository
+public interface RentalDao extends JpaRepository<Rental, Integer> {
+
+	Rental getByCarCarId(int id);
+}
