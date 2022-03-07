2cb970980376b352573f1a918cd215e5b6c231d5
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-03-07 18:20:18 +0300

    updated controllers

---
+++ rentacar/src/main/java/com/omertdemirel/rentacar/api/controller/RentalController.java
@@ -0,0 +1,63 @@
+package com.omertdemirel.rentacar.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.omertdemirel.rentacar.business.abstracts.RentalService;
+import com.omertdemirel.rentacar.business.dtos.ListRentalDto;
+import com.omertdemirel.rentacar.business.dtos.RentalDto;
+import com.omertdemirel.rentacar.business.request.CreateRentalRequest;
+import com.omertdemirel.rentacar.business.request.DeleteRentalRequest;
+import com.omertdemirel.rentacar.business.request.UpdateRentalRequest;
+import com.omertdemirel.rentacar.core.utilities.results.DataResult;
+import com.omertdemirel.rentacar.core.utilities.results.Result;
+
+
+@RestController
+@RequestMapping("/api/rentals")
+public class RentalController {
+
+	private final RentalService rentalService;
+
+	@Autowired
+	public RentalController(RentalService rentalService) {
+		this.rentalService = rentalService;
+	}
+
+	@GetMapping("/getAll")
+	public DataResult<List<ListRentalDto>> getAll() {
+		return rentalService.getAll();
+	}
+
+	@GetMapping("/get/{id}")
+	public DataResult<RentalDto> get(@RequestParam int id) {
+		return rentalService.getById(id);
+	}
+
+	@PostMapping("/save")
+	public Result add(@RequestBody @Valid CreateRentalRequest createRentalRequest) {
+		return rentalService.add(createRentalRequest);
+	}
+
+	@DeleteMapping("/delete")
+	public Result delete(@RequestBody @Valid DeleteRentalRequest deleteRentalRequest) {
+		return rentalService.delete(deleteRentalRequest);
+	}
+
+	@PutMapping("/update")
+	public Result delete(@RequestBody @Valid UpdateRentalRequest updateRentalRequest) {
+		return rentalService.update(updateRentalRequest);
+	}
+
+}
