2dc0091a36753998198ab82a2695e685a8e750f7
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-03-11 18:54:04 +0300

    added AdditonalServiceDao

---
+++ rentacar/src/main/java/com/omertdemirel/rentacar/dataAccess/abstracts/AdditionalServiceDao.java
@@ -0,0 +1,12 @@
+package com.omertdemirel.rentacar.dataAccess.abstracts;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.omertdemirel.rentacar.entities.concretes.AdditionalService;
+
+@Repository
+public interface AdditionalServiceDao extends JpaRepository<AdditionalService, Integer> {
+	List<AdditionalService> getAllByRentId(int id);
+}
