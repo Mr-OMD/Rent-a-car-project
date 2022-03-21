522251e29e889571f9cc326265ae61830efa7e4e
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-03-21 18:34:47 +0300

    added entities

---
+++ rentacar/src/main/java/com/omertdemirel/rentacar/entities/concretes/Invoice.java
@@ -0,0 +1,59 @@
+package com.omertdemirel.rentacar.entities.concretes;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.omertdemirel.rentacar.entities.abstracts.Customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Invoices")
+@Entity
+public class Invoice {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "invoice_no")
+	private int invoiceNo;
+
+	@Column(name = "billing_date")
+	private LocalDate billingDate;
+
+	@Column(name = "rent_date")
+	private LocalDate rentDate;
+
+	@Column(name = "return_date")
+	private LocalDate returnDate;
+
+	@Column(name = "total_rent_day")
+	private long totalRentDay;
+
+	@Column(name = "total_price")
+	private BigDecimal totalPrice;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private Customer customer;
+
+	@OneToOne()
+	@JoinColumn(name = "rent_id")
+	private Rental rental;
+
+	@OneToOne(mappedBy = "invoice")
+	private Payment payment;
+
+}
