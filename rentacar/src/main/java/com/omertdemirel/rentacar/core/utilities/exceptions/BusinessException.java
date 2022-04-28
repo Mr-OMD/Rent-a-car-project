96812fc7ef563d9ddee0440a5daf1a3cc87958a9
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-04-29 01:20:21 +0300

    fixed core exception

--- rentacar/src/main/java/com/omertdemirel/rentacar/core/utilities/exceptions/BusinessException.java
+++ rentacar/src/main/java/com/omertdemirel/rentacar/core/utilities/exceptions/BusinessException.java
@@ -1,6 +1,7 @@
 package com.omertdemirel.rentacar.core.utilities.exceptions;
 
-public class BusinessException extends Exception {
+public class BusinessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 	public BusinessException(String message) {
 		super(message);
 	}
