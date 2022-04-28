c972f06b29aa67f1b2207acd2ab1a16b9ca4f33a
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-04-29 02:23:55 +0300

    constant messages added

---
+++ rentacar/src/main/java/com/omertdemirel/rentacar/business/constants/Messages.java
@@ -0,0 +1,133 @@
+package com.omertdemirel.rentacar.business.constants;
+
+public class Messages {
+
+	public static final String VALIDATIONERRORS = "Validation Errors";
+	public static final String BUSINESSEXCEPTIONERRORS = "BusinessException Errors";
+	public static final String SUCCESS = "Success";
+	public static final String DATAINPAGE = "Data in page ";
+	public static final String ISLISTEDWITHDATASIZE = " is listed with respect to data size ";
+	public static final String DATALISTEDIN = "Data listed in ";
+	public static final String ORDER = " order";
+	public static final String AND = " and ";
+	public static final String DATABETWEEN = "Data between ";
+	public static final String ISLISTED = " is listed";
+	public static final String EMAILEXISTS = "This Customer Email is already exists!";
+	public static final String CUSTOMERNOTFOUND = "This Customer Id is undefined!";
+	public static final String CUSTOMERANDCARDDOESNOTMATCH = "This Customer is not permit to Rent A Car with this Credit Card!";
+	public static final String PAGENOCANNOTLESSTHANZERO = "Page number must be higher than zero(0)!";
+	public static final String PAGESIZECANNOTLESSTHANZERO = "Page size must be higher than zero(0)!";
+	public static final String JSONPARSEERRORS = "Sent JSON data is readable!";
+	
+	public static final String ADDITIONALSERVICEADDED = "AdditionalService Added: ";
+	public static final String ADDITIONALSERVICEUPDATED = "AdditionalService Updated: ";
+	public static final String ADDITIONALSERVICEDELETED = "AdditonalService Deleted: ";
+	public static final String ADDITIONALSERVICESLISTED = "AdditionalServices Listed";
+	public static final String ADDITIONALSERVICEGETTEDBYID = "AdditionalService getted by Id";
+	public static final String ADDITIONALSERVICEEXISTS = "This AdditionalService is already exists: ";
+	public static final String ADDITIONALSERVICENOTFOUND = "This AdditionalService Id is undefined";
+	
+	public static final String BRANDADDED = "Brand Added: ";
+	public static final String BRANDUPDATED = "Brand Updated: ";
+	public static final String BRANDDELETED = "Brand Deleted: ";
+	public static final String BRANDSLISTED = "Brands Listed";
+	public static final String BRANDGETTEDBYID = "Brand getted by Id";
+	public static final String BRANDNOTFOUND = "This Brand Id is undefined!";
+	public static final String BRANDEXISTS = "This Brand is already exists: ";
+	
+	public static final String CARMAINTENANCEADDED = "CarMaintenance Added: ";
+	public static final String CARMAINTENANCEUPDATED = "CarMaintenance Updated: ";
+	public static final String CARMAINTENANCEDELETED = "CarMaintenance Deleted: ";
+	public static final String CARMAINTENANCESLISTED = "CarMaintenances Listed";
+	public static final String CARMAINTENANCEGETTEDBYID = "CarMaintenance getted by Id";
+	public static final String CARMAINTENANCENOTFOUND = "This CarMaintenance Id is undefined";
+	public static final String CARMAINTENANCECARIDNOTFOUND = "Car is not found!";
+	public static final String CARMAINTENANCESTILLMAINTENANCED = "Car is already in maintenance!";
+	public static final String CARMAINTENANCESGETTEDBYCARID = "CarMaintenances listed by Car Id";
+	
+	public static final String CARADDED = "Car Added: ";
+	public static final String CARUPDATED = "Car Updated: ";
+	public static final String CARDELETED = "Car Deleted: ";
+	public static final String CARSLISTED = "Cars Listed";
+	public static final String CARGETTEDBYID = "Car getted by Id";
+	public static final String CARLISTEDRESPECTTODAILYPRICE = "Cars listed with respect to daily price less than or equal to ";
+	public static final String CARNOTFOUND = "This Car Id is undefined!";
+	
+	public static final String CITIESLISTED = "Cities listed";
+	public static final String CITYGETTEDBYPLATE = "City getted by Plate";
+	public static final String CITYNOTFOUND = "This City Plate is undefined!";
+	
+	public static final String COLORADDED = "Color Added: ";
+	public static final String COLORUPDATED = "Color Updated: ";
+	public static final String COLORDELETED = "Color Deleted: ";
+	public static final String COLORSLISTED = "Colors Listed";
+	public static final String COLORGETTEDBYID = "Color getted by Id";
+	public static final String COLOREXISTS = "This Color is already exists: ";
+	public static final String COLORNOTFOUND = "This Color Id is undefined!";
+	
+	public static final String CORPORATECUSTOMERADDED = "CorporateCustomer Added: ";
+	public static final String CORPORATECUSTOMERUPDATED = "CorporateCustomer Updated: ";
+	public static final String CORPORATECUSTOMERDELETED = "CorporateCustomer Deleted: ";
+	public static final String CORPORATECUSTOMERSLISTED = "CorporateCustomers Listed";
+	public static final String CORPORATECUSTOMERGETTEDBYID = "CorporateCustomer getted by Id";
+	public static final String CORPORATECUSTOMERNOTFOUND = "This CorporateCustomer Id is undefined!";
+	public static final String CORPORATECUSTOMERTAXNOEXISTS = "This Tax Number is already exists: ";
+	public static final String CORPORATECUSTOMEREXISTS = "This CorporateCustomer is already exists: ";
+	public static final String CORPORATECUSTOMERTAXNOCONTAINSLETTER = "CorporateCustomer TaxNo cannot contains any letter";
+	
+	public static final String CREDITCARDADDED = "CreditCard Added";
+	public static final String CREDITCARDUPDATED = "CreditCard Updated";
+	public static final String CREDITCARDDELETED = "CreditCard Deleted";
+	public static final String CREDITCARDSLISTED = "CreditCards Listed";
+	public static final String CREDITCARDGETTEDBYID = "CreditCard getted by Id";
+	public static final String CREDITCARDGETTEDBYCREDITCARDNUMBER = "CreditCard getted by CreditCardNumber";
+	public static final String CREDITCARDNOTFOUND = "This CreditCard is undefined!";
+	public static final String CREDITCARDISEXISTS = "This CreditCard is already exists!";
+	public static final String CREDITCARDNOTVALID = "This CreditCard is not Valid!";
+	
+	public static final String INDIVIDUALCUSTOMERADDED = "IndividualCustomer Added: ";
+	public static final String INDIVIDUALCUSTOMERUPDATED = "IndividualCustomer Updated: ";
+	public static final String INDIVIDUALCUSTOMERDELETED = "IndividualCustomer Deleted: ";
+	public static final String INDIVIDUALCUSTOMERSLISTED = "IndividualCustomers Listed";
+	public static final String INDIVIDUALCUSTOMERGETTEDBYID = "IndividualCustomer getted by Id";
+	public static final String INDIVIDUALCUSTOMERNOTFOUND = "This IndividualCustomer Id is undefined!";
+	public static final String INDIVIDUALCUSTOMERIDENTITYEXISTS = "This National Identity is already exists: ";
+	public static final String INDIVIDUALCUSTOMERIDENTITYNOCONTAINSLETTER = "IndividualCustomer IdentityNo cannot contains any letter";
+	
+	public static final String DAMAGEADDED = "Damage Added: ";
+	public static final String DAMAGEUPDATED = "Damage Updated: ";
+	public static final String DAMAGEDELETED = "Damage Deleted: ";
+	public static final String DAMAGESLISTED = "Damages Listed";
+	public static final String DAMAGEGETTEDBYID = "Damage getted by Id";
+	public static final String DAMAGENOTFOUND = "This Damage Id is undefined!";
+	
+	public static final String INVOICEADDED = "Invoice Added: ";
+	public static final String INVOICEUPDATED = "Invoice Updated: ";
+	public static final String INVOICEDELETED = "Invoice Deleted";
+	public static final String INVOICESLISTED = "Invoices Listed";
+	public static final String INVOICEGETTEDBYID = "Invoice getted by Id";
+	public static final String INVOICEGETTEDBYNUMBER = "Invoice getted by InvoiceNumber";
+	public static final String INVOICESGETTEDBYCUSTOMERID = "Invoices listed with respect to Customer Id: ";
+	public static final String INVOICENOTFOUND = "This Invoice Id is undefined!";
+	public static final String INVOICENOTFOUNDBYNUMBER = "This Invoice Number is undefined!";
+	public static final String INVOICECUSTOMERRENTNOTFOUND = "Customer has not already Rent any Car";
+	
+	public static final String PAYMENTADDED = "Payment Added";
+	public static final String PAYMENTUPDATED = "Payment Updated";
+	public static final String PAYMENTDELETED = "Payment Deleted";
+	public static final String PAYMENTSLISTED = "Payments Listed";
+	public static final String PAYMENTGETTEDBYID = "Payment getted by Id";
+	public static final String PAYMENTNOTFOUND = "This Payment Id is undefined!";
+	public static final String PAYMENTMETHODNOTVALID = "This Payment method is not valid!";
+	
+	public static final String RENTALADDED = "Rental Added ";
+	public static final String RENTALUPDATED = "Rental Updated: ";
+	public static final String RENTALDELETED = "Rental Deleted: ";
+	public static final String RENTALSLISTED = "Rentals Listed";
+	public static final String RENTALGETTEDBYID = "Rental getted by Id";
+	public static final String RENTALNOTFOUND = "This Rental Id is undefined!";
+	public static final String RENTALNOTFOUNDBYCAR = "Car is not found!";
+	public static final String RENTALCARINRENT = "Car is already in Rent!";
+	public static final String RENTALCARKILOMETER = "Kilometer of Car cannot be less than Rented Kilometer!";
+	public static final String RENTALTOTALPRICE = " total price is: ";
+}
