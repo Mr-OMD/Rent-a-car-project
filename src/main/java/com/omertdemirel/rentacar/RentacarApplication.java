430f8d8df88dab2e8cb37d784f2284606dcf8241
Ömer Mert Demirel <demirel.omert@gmail.com>
2022-04-29 14:47:39 +0300

    exception handling fix

--- src/main/java/com/omertdemirel/rentacar/RentacarApplication.java
+++ src/main/java/com/omertdemirel/rentacar/RentacarApplication.java
@@ -8,12 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.omertdemirel.rentacar.business.constants.Messages;
 import com.omertdemirel.rentacar.core.utilities.exceptions.BusinessException;
 import com.omertdemirel.rentacar.core.utilities.results.ErrorDataResult;
 
@@ -32,22 +34,32 @@ public class RentacarApplication {
 
 	@ExceptionHandler
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-	public ErrorDataResult<Object> handleValidationExceptions(MethodArgumentNotValidException argumentNotValidException) {
+	public ErrorDataResult<Object> handleValidationExceptions(
+			MethodArgumentNotValidException argumentNotValidException) {
 		Map<String, String> validationErrors = new HashMap<String, String>();
 		for (FieldError fieldError : argumentNotValidException.getBindingResult().getFieldErrors()) {
 			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
 		}
-		ErrorDataResult<Object> errorDataResult = new ErrorDataResult<Object>(validationErrors, "Validation.Error");
+
+		ErrorDataResult<Object> errorDataResult = new ErrorDataResult<Object>(validationErrors, Messages.VALIDATIONERRORS);
 		return errorDataResult;
+	}
+	
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleHttpMessageNotReadableException(
+			HttpMessageNotReadableException httpMessageNotReadableException){
+		ErrorDataResult<Object> errorJsonResult = new ErrorDataResult<Object>(httpMessageNotReadableException.getMessage(),
+				Messages.JSONPARSEERRORS);
+		return errorJsonResult;
 	}
 
 	@ExceptionHandler
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	public ErrorDataResult<Object> handleBusinessExceptions(BusinessException businessException) {
 		ErrorDataResult<Object> errorDataResult = new ErrorDataResult<Object>(businessException.getMessage(),
-				"Business.Exception.Errors");
+				Messages.BUSINESSEXCEPTIONERRORS);
 		return errorDataResult;
-
 	}
 
 }
